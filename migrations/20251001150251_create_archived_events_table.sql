c662447dcb4814cc608fd0fb61b9cf178ae99bdc
Aliskhan <aliskhanreventon@gmail.com>
2025-10-01 18:24:50 +0300

    add migration for creating archived events table

---
+++ migrations/20251001150251_create_archived_events_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS archived_events
+(
+    id          UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id     UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    event_date  DATE NOT NULL,
+    title       TEXT NOT NULL,
+    description TEXT,
+    created_at  TIMESTAMP        DEFAULT now(),
+    updated_at  TIMESTAMP        DEFAULT now()
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS archived_events;
+-- +goose StatementEnd
